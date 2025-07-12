c8cdbb100c4a7cdd5e77d877fb26bc588ca30a59
Cewen Zheng <98297318+cubaneOvO@users.noreply.github.com>
2025-07-12 18:25:22 +0800

    Update etcd-cpp-api_install.sh

--- scripts/etcd-cpp-api_install.sh
+++ scripts/etcd-cpp-api_install.sh
@@ -1,8 +1,14 @@
+mkdir -p ../deploy/bins
+mkdir -p ../deploy/libs
+mkdir -p ../deploy/include
+mkdir -p ../deploy/log
+
 sudo apt-get install libboost-all-dev libssl-dev 
 sudo apt-get install libprotobuf-dev protobuf-compiler-grpc 
 sudo apt-get install libgrpc-dev libgrpc++-dev  
 sudo apt-get install libcpprest-dev
 
+mkdir ../3rdparty
 cd ../3rdparty
 git clone https://gitcode.com/gh_mirrors/et/etcd-cpp-apiv3.git
 cd etcd-cpp-apiv3 
